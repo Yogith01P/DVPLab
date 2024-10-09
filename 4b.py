08e875866cbd105fd0dd341bb375e6a6ddcdf1b6
Yogith01P <yogithyogith27@gmail.com>
2024-10-09 00:40:39 -0700

    Add files via upload

---
+++ 4b.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+x=[1,2,3,4,5]
+y=[-1,0,1,2,3]
+plt.scatter(x,y,color='green')
+plt.xlabel("Education",fontsize=12)
+plt.ylabel("Marks obtained",fontsize=12)
+plt.title("YOGITH-1KI23CS188")
+plt.show()
