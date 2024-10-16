fb4bf249d32b3132a0bd517b8e47b45a409392a3
Yogith01P <yogithyogith27@gmail.com>
2024-10-16 12:31:19 +0530

    Add files via upload

---
+++ 5a.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as  plt
+plt.hist([35,48,75,16,43,71,23,28,22,38,34,32],bins=4)
+plt.xlabel("Distribution of age",fontsize=16)
+plt.ylabel("Frequency",fontsize=16)
+plt.title("YOGITH.P-1KI23CS188")
+plt.show()
