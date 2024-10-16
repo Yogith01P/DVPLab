d87e1604de26cdd41aae85e19d962152531dbbf6
Yogith01P <yogithyogith27@gmail.com>
2024-10-16 12:45:07 +0530

    Add files via upload

---
+++ 5b.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as  plt
+CARS=['BMW','Audi','Ferrari','Tusla','Jaguar','Toyata']
+data=['25','15','10','11','9','40']
+plt.figure(figsize=(10,7))
+plt.pie(data,labels=CARS)
+plt.title("YOGITH.P-1KI23CS188")
+plt.show()
