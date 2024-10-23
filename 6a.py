d8d8940ba4b40113f933162118211be6002acc90
Yogith01P <yogithyogith27@gmail.com>
2024-10-23 00:37:24 -0700

    Add files via upload

---
+++ 6a.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import numpy as np
+x=np.linspace(1,20,7)
+y1=x
+y2=np.square(x)
+y3=np.sqrt(x)
+print(y1)
+print(y2)
+print(y3)
+plt.plot(x,y1,'red',x,y2,'green',x,y3,'blue')
+plt.legend(['x','square(x)','sqrt(x)'])
+plt.xlabel("x-axis")
+plt.ylabel("y-axis")
+plt.xticks(x)
+plt.yticks(y2)
+plt.title("Linear-YOGITH.P-1KI23CS188")
+plt.show()
