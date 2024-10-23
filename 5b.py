f941d45a22f70098526f563abc52c826bab161d5
Yogith01P <yogithyogith27@gmail.com>
2024-10-23 00:40:21 -0700

    Delete 5b.py

--- 5b.py
+++
@@ -1,7 +0,0 @@
-import matplotlib.pyplot as  plt
-CARS=['BMW','Audi','Ferrari','Tesla','Jaguar','Toyata']
-data=['25','15','10','11','9','40']
-plt.figure(figsize=(10,7))
-plt.pie(data,labels=CARS)
-plt.title("YOGITH.P-1KI23CS188")
-plt.show()
