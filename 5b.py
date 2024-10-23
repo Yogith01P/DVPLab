eb6a9046e9d07da7bde699a3dc3ce10ba7082cf7
Yogith01P <yogithyogith27@gmail.com>
2024-10-23 06:45:21 +0530

    5b.py

--- 5b.py
+++ 5b.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as  plt
-CARS=['BMW','Audi','Ferrari','Tusla','Jaguar','Toyata']
+CARS=['BMW','Audi','Ferrari','Tesla','Jaguar','Toyata']
 data=['25','15','10','11','9','40']
 plt.figure(figsize=(10,7))
 plt.pie(data,labels=CARS)
